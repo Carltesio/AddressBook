39c737c8419aa0304d41b3f976259e066b3ea205
carlos <carlos.delgado@hotmail.it>
2020-01-28 13:04:08 +0100

    creating new branch address_book_function,adding submit button and test

--- cypress/integration/userCanCreateAContent.feature.js
+++ cypress/integration/userCanCreateAContent.feature.js
@@ -6,7 +6,15 @@ describe('user can create a contact', () => {
             cy.get('#email').type('thomas@craft.se')
             cy.get('#phone').type('0700 101010')
             cy.get('#company').type('Craft Academy')
-            cy.get('#twitter').type('@thomasochman')
             cy.get('#notes').type('Awesome coder')
+            cy.get('#twitter').type('@thomasochman')
+            cy.get('#submit').click()
        })
+       it('displays a name of the contact', () => {
+            cy.get('#contact-list').should('contain', 'Thomas')
+       })
+
+       it('displays a name of the contact', () => {
+        cy.get('#contact-list').should('contain', '0700 101010')
+   })
 })
